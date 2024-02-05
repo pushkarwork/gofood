88b0e8f1f1257d242abd7257973cd2f65262093a
pushkar verma <pushkar753@gmail.com>
2024-02-05 10:28:55 +0530

    rrd install

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './screens/Home'
+
+const App = () => {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  )
+}
+
+export default App
---
+++ src/screens/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            this is body
+            <Footer></Footer>
+
+        </div>
+    )
+}
+
+export default Home
